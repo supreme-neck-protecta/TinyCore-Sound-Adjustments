c0942068b0fe67743b34cd42471d4070abd24d53
Edward <40375833+dynobot@users.noreply.github.com>
2019-02-01 21:51:29 -0700

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+border()
+{
+    local title="| $1 |"
+    local edge=${title//?/-}
+    echo -e "${edge}\n${title}\n${edge}"
+    sleep 1
+}
+
+border 'Removing Sound Adjustments'
+sudo rm /opt/bootlocal.sh
+sudo mv /opt/bootlocal.sh.bak /opt/bootlocal.sh
+sudo filetool.sh -b
+
+border 'Rebooting System'
+
