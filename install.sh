2415c711aa221508c104c17dd2c5e5bd7ee31993
Edward <40375833+dynobot@users.noreply.github.com>
2019-02-01 21:01:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ border()
 
 border 'Downloading Sound File'
 
-wget https://github.com/dynobot/TinyCore-Sound-Adjustments/raw/master/bootlocal.sh
+wget https://github.com/dynobot/TinyCore-Sound-Adjustments/raw/master/bootlocal.sh -O /opt/bootlocal.sh
 mv /opt/bootlocal.sh /opt/bootlocal.sh.bak
 mv bootlocal.sh /opt/bootlocal.sh
 sudo chmod 755 bootlocal.sh
