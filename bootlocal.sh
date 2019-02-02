8412bc6f04dd05898bc7753d1b189b431605bd3b
Edward <40375833+dynobot@users.noreply.github.com>
2019-02-01 20:35:29 -0700

    Create bootlocal.sh
    For PiCorePlayer

---
+++ bootlocal.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# put other system startup commands here
+
+#Reduce Audio thread latency
+chrt -f -p 57 $(pidof ksoftirqd/0)
+chrt -f -p 57 $(pidof ksoftirqd/1)
+chrt -f -p 57 $(pidof ksoftirqd/2)
+chrt -f -p 57 $(pidof ksoftirqd/3)
+
+#Reduce operating system latency
+echo noop > /sys/block/mmcblk0/queue/scheduler
+echo 1000000 > /proc/sys/kernel/sched_latency_ns
+echo 100000 > /proc/sys/kernel/sched_min_granularity_ns
+echo 25000 > /proc/sys/kernel/sched_wakeup_granularity_ns
+
+#Squeezelite FIFO priority
+chrt -f -p 87 $(pidof squeezelite)
+
+GREEN="$(echo -e '\033[1;32m')"
+
+echo
+echo "${GREEN}Running bootlocal.sh..."
+#pCPstart------
+/home/tc/www/cgi-bin/pcp_startup.sh 2>&1 | tee -a /var/log/pcp_boot.log
+#pCPstop------
+~
