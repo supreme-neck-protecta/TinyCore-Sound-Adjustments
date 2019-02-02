6e63dd7bc708feedee9a11db67ff4d8953e19bd7
Edward <40375833+dynobot@users.noreply.github.com>
2019-02-01 21:47:50 -0700

    Update bootlocal.sh

--- bootlocal.sh
+++ bootlocal.sh
@@ -13,9 +13,6 @@ echo 1000000 > /proc/sys/kernel/sched_latency_ns
 echo 100000 > /proc/sys/kernel/sched_min_granularity_ns
 echo 25000 > /proc/sys/kernel/sched_wakeup_granularity_ns
 
-#Squeezelite FIFO priority
-chrt -f -p 43 $(pidof squeezelite)
-
 GREEN="$(echo -e '\033[1;32m')"
 
 echo
@@ -23,4 +20,8 @@ echo "${GREEN}Running bootlocal.sh..."
 #pCPstart------
 /home/tc/www/cgi-bin/pcp_startup.sh 2>&1 | tee -a /var/log/pcp_boot.log
 #pCPstop------
+
+#Squeezelite FIFO priority
+sleep 5
+chrt -f -p 43 $(pidof squeezelite)
 ~
