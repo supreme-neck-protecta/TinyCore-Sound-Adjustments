bc9c2c24fa833ffbfc043eecf6384c1edc496fff
Edward <40375833+dynobot@users.noreply.github.com>
2020-08-02 16:02:40 -0700

    Update bootlocal.sh

--- bootlocal.sh
+++ bootlocal.sh
@@ -3,19 +3,15 @@
 
 GREEN="$(echo -e '\033[1;32m')"
 
-#Reduce Audio thread latency
-sudo chrt -f -p 43 $(pidof ksoftirqd/0)
-sudo chrt -f -p 43 $(pidof ksoftirqd/1)
-sudo chrt -f -p 43 $(pidof ksoftirqd/2)
-sudo chrt -f -p 43 $(pidof ksoftirqd/3)
-
 echo
 echo "${GREEN}Running bootlocal.sh..."
 #pCPstart------
 /home/tc/www/cgi-bin/pcp_startup.sh 2>&1 | tee -a /var/log/pcp_boot.log
 #pCPstop------
 
-#Squeezelite FIFO priority
-#sleep 5
-#sudo chrt -f -p 27 $(pidof squeezelite)
-~
+#Reduce Audio thread latency
+sudo chrt -f -p 43 $(pidof ksoftirqd/0)
+sudo chrt -f -p 43 $(pidof ksoftirqd/1)
+sudo chrt -f -p 43 $(pidof ksoftirqd/2)
+sudo chrt -f -p 43 $(pidof ksoftirqd/3)
+
