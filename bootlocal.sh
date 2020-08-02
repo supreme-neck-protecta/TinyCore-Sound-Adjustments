6bece5f0848d97e715ca147a5915fc0972398405
Edward <40375833+dynobot@users.noreply.github.com>
2020-08-02 15:56:56 -0700

    Update bootlocal.sh

--- bootlocal.sh
+++ bootlocal.sh
@@ -3,13 +3,11 @@
 
 GREEN="$(echo -e '\033[1;32m')"
 
-
 #Reduce Audio thread latency
 sudo chrt -f -p 43 $(pidof ksoftirqd/0)
 sudo chrt -f -p 43 $(pidof ksoftirqd/1)
 sudo chrt -f -p 43 $(pidof ksoftirqd/2)
 sudo chrt -f -p 43 $(pidof ksoftirqd/3)
-
 
 echo
 echo "${GREEN}Running bootlocal.sh..."
